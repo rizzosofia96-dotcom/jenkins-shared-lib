d31450e4bb5dfd905e9185912158aca0d0ba6765
rizzosofia96-dotcom <rizzosofia96@gmail.com>
2025-12-05 16:22:15 +0100

    Update Bar.groovy

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -1,21 +1,22 @@
 package org.foo
-
 class Bar implements Serializable {
-
     def steps
 
     Bar(steps) {
         this.steps = steps
     }
 
-    void analyzeRepo(String repoName) {
-        steps.echo "Cloning repo: ${repoName}"
-        def repoUrl = "https://github.com/rizzosofia96-dotcom/Sommaduenumeri"
-        
-        steps.dir(repoName) {
-            steps.checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[url: repoUrl]]])
-            steps.echo "Analisi del repository ${repoName} in corso..."
-            // Qui metti la tua logica di analisi
+    def cloneAndAnalyze(String repoUrl, String branch = 'main') {
+        steps.echo "Cloning repo ${repoUrl} branch ${branch}"
+        steps.checkout([$class: 'GitSCM',
+                       branches: [[name: branch]],
+                       userRemoteConfigs: [[url: repoUrl]]
+        ])
+
+        steps.echo "Running SonarQube analysis"
+        steps.withSonarQubeEnv('sonarqube') {  // nome server Sonar configurato in Jenkins
+            steps.sh 'sonar-scanner -Dsonar.projectKey=Gestionerubrica -Dsonar.sources=.'
         }
     }
 }
+
