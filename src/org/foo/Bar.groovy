b23a481c273ad58d3f308cdbd340f94c3140ba67
rizzosofia96-dotcom <rizzosofia96@gmail.com>
2025-12-05 16:47:06 +0100

    Update Bar.groovy

--- src/org/foo/Bar.groovy
+++ src/org/foo/Bar.groovy
@@ -1,4 +1,5 @@
 package org.foo
+
 class Bar implements Serializable {
     def steps
 
@@ -7,16 +8,28 @@ class Bar implements Serializable {
     }
 
     def cloneAndAnalyze(String repoUrl, String branch = 'main') {
+        // Stampa un messaggio di log che indica quale repo e branch verranno clonati
         steps.echo "Cloning repo ${repoUrl} branch ${branch}"
-        steps.checkout([$class: 'GitSCM',
-                       branches: [[name: branch]],
-                       userRemoteConfigs: [[url: repoUrl]]
+
+        // Effettua il checkout del repository Git specificato, sul branch indicato
+        steps.checkout([
+            $class: 'GitSCM',
+            branches: [[name: branch]],
+            userRemoteConfigs: [[url: repoUrl]]
         ])
 
+        // Stampa un messaggio di log per indicare l'inizio dell'analisi SonarQube
         steps.echo "Running SonarQube analysis"
-        steps.withSonarQubeEnv('sonarqube') {  // nome server Sonar configurato in Jenkins
-            steps.sh 'sonar-scanner -Dsonar.projectKey=Gestionerubrica -Dsonar.sources=.'
+
+        // Avvia il blocco con l'ambiente di SonarQube configurato su Jenkins con il nome 'sonarqube'
+        steps.withSonarQubeEnv('sonarqube') {
+            // Esegue il comando sonar-scanner in ambiente Windows tramite 'bat'
+            steps.bat '''
+                sonar-scanner ^
+                  -Dsonar.projectKey=Gestionerubrica ^
+                  -Dsonar.sources=. ^
+                  -Dsonar.java.binaries=.
+            '''
         }
     }
 }
-
